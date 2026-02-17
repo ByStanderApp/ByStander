a15fcc9148a3ab33c3d2b5380f88d09a877d9e0a
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2026-02-18 00:47:37 +0700

    Update plugin registrations and Google Maps API key in web index.html

--- bystander_frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ bystander_frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,9 +9,8 @@ import audioplayers_darwin
 import cloud_firestore
 import firebase_auth
 import firebase_core
-
-import google_sign_in_ios
 import geolocator_apple
+import google_sign_in_ios
 import path_provider_foundation
 import speech_to_text_macos
 import url_launcher_macos
@@ -21,8 +20,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
